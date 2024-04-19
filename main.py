d3f85a062a8ed5249577fff37fa72cbe7cfa0530
customgunz00 <159488157+customgunz00@users.noreply.github.com>
2024-04-19 17:14:32 -0600

    asdf

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import subprocess
 import platform
 
 osname = platform.system()
+command = None
 
+if osname == 'Windows':
+    command = ["powershell", "-ExecutionPolicy", "Bypass", "-File"]
+    command = ["powershell", "./win.ps1"]
+    subprocess.run(command, check=False)
