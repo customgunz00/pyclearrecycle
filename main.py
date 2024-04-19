6110c9c88fb5e7daa6eadcc1354b31f5587eeb2d
customgunz00 <159488157+customgunz00@users.noreply.github.com>
2024-04-19 16:49:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
 import subprocess
-
-def clear_recycle_bin():
-  try:
-    command = ["powershell", "-Command", "Clear-RecycleBin -Force"]
-    subprocess.run(command, check=False)
-  except subprocess.CalledProcessError:
-    pass
-
-clear_recycle_bin()
-print("Recycle Bin Cleared (if it had any contents)")
+import platform
